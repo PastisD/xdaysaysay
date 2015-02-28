956a6fb33ff5a3767c5cc912fc00d4f0507391aa
PastisD <PastisD@gmail.com>
2015-02-28 18:25:48 +0100

    Front : Show only teams which are allowed in front in search

--- www/web/system/pages/search.php
+++ www/web/system/pages/search.php
@@ -4,15 +4,6 @@ $template->set_filenames( array(
  'search' => 'search.tpl'
 ) );
 
-//$id_xdcc = (int) $_GET['id_xdcc'];
-//$id_team = (int) $_GET['id_team'];
-
-//$team = $teams[ $id_team ];
-//$template->assign_block_vars( 'team', array(
-// 'NAME' => $team->chan_name . "@" . $team->host,
-// 'IRC' => "irc://" . $team->host . ":" . $team->port . "/" . str_replace( "#", "", $team->chan_name),
-//) );
-
 $levenstein = array();
 $query = "SELECT DISTINCT *
           FROM levenstein;";
@@ -26,7 +17,6 @@ $query = "SELECT DISTINCT *
     if( $number <= 3 )
     {
      $levenstein[] = $obj->name;
-//     echo $obj->name . " : " . $number . "<br />";
     }
    }
   }
@@ -47,14 +37,16 @@ if( !is_null( $pattern ) && !empty( $pattern ))
            FROM packs p
            LEFT JOIN xdccs x ON ( x.id = p.id_xdcc )
            LEFT JOIN xdcc_irc_server xis ON ( xis.id_xdcc = x.id )
-           LEFT JOIN teams t ON ( t.id = x.id_team )
-           WHERE p.name LIKE ('%" . str_replace(" ", "%", $pattern ) . "%')";
+           LEFT JOIN team_xdcc tx ON (tx.id_xdcc = x.id) 
+           LEFT JOIN teams t ON ( t.id = tx.id_team )
+           WHERE p.name LIKE ('%" . str_replace(" ", "%", $pattern ) . "%')
+             AND t.show_on_listing = '1'
+             AND x.show_on_listing = '1'";
  $query .= ( !is_null( $id_team ) && isset( $teams[ $id_team ] ) ) ? "  AND t.id = " . $id_team . " " : " ";
  $query .= "ORDER BY t.name, xis.name_xdcc, p.id_pack;";
  $teams_search = array();
  if ( $result = $mysqli->query( $query ) )
  {
- // $i = 0;
   while ( $obj = $result->fetch_object() )
   {
    $teams_search[ $obj->id_team ][ $obj->id_xdcc ][ $obj->id_pack ] = $obj;
